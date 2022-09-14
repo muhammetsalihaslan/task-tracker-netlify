4d3810dae5d3698b2993c917651174abeb092504
salih <muhammetsalihaslan@gmail.com>
2022-09-14 17:56:30 +0300

    first commit

---
+++ src/components/ShowTasks.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import {TiTrash} from "react-icons/ti";
+
+
+const ShowTasks = ({ tasks, setTasks}) => {
+
+  const [done, setDone] = useState(true)
+
+ const handleDelete = (id) => {
+  console.log(id);
+  setTasks(tasks.filter((item) => item.id !== id));
+ };
+
+ 
+ const handleDone = (e) => {
+  if(done){
+    e.target.classList.add('done');
+    setDone(!done);
+    console.log(e.target);
+  } else {
+    e.target.classList.remove('done');
+    setDone(!done);
+    console.log(e.target);
+  }
+ }
+
+
+
+ 
+ 
+
+  return(
+    
+    tasks?.map((item) => {
+      const {id, task, day, isDone} = item;
+      return ( 
+        <div className="container">
+          <div key={id} className="btn-tasks" onClick={handleDone} >
+            <div className="task-area">
+              <h3>{task}</h3>
+              <p>{day}</p>
+            </div>
+            <div className="btn-delete" style={{cursor: 'pointer'}} onClick={() => handleDelete(id)}>
+              <TiTrash size={35}/>
+            </div>
+          </div>
+    
+        </div>
+      )
+  
+    })
+  );
+};
+
+export default ShowTasks;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Header from "../components/Header";
+import ShowTasks from "../components/ShowTasks";
+// import data from "../helper/starterData";
+
+
+const Home = () => {
+    const [ tasks, setTasks] = useState(
+        JSON.parse(localStorage.getItem('tasks')) || []
+    );
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+    
+    return(
+        <div>
+            <Header tasks={tasks} setTasks={setTasks}/>
+            <ShowTasks tasks={tasks} setTasks={setTasks}/>
+        </div>
+    );
+};
+
+export default Home;
